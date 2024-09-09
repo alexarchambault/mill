7f878e8df3dc1fab82e45a4a3b7195a31a327f8a
Alexandre Archambault <alexandre.archambault@gmail.com>
2024-09-09 20:07:50 +0200

    fixup

--- .github/scripts/coursier-snapshot.sh
+++ .github/scripts/coursier-snapshot.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-git clone https://github.com/alexarchambault/coursier.git -b debug-mill-ci-windows
-cd coursier
-git fetch --tags
-git submodule update --init --recursive
-./mill -i 'core.jvm[2.13.12].publishLocal' +\
-  'cache.jvm[2.13.12].publishLocal' +\
-  'proxy-setup.publishLocal' +\
-  'util.jvm[2.13.12].publishLocal' +\
-  'coursier.jvm[2.13.12].publishLocal'
-cd -
+if ! test -d coursier; then
+  git clone https://github.com/alexarchambault/coursier.git -b debug-mill-ci-windows
+  cd coursier
+  git fetch --tags
+  git submodule update --init --recursive
+  ./mill -i 'core.jvm[2.13.12].publishLocal' +\
+    'cache.jvm[2.13.12].publishLocal' +\
+    'proxy-setup.publishLocal' +\
+    'util.jvm[2.13.12].publishLocal' +\
+    'coursier.jvm[2.13.12].publishLocal'
+  cd -
+fi
