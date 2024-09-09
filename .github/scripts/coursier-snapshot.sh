b9a0e6f713842d96e1ad6e28e0cb88503cb55035
Alexandre Archambault <alexandre.archambault@gmail.com>
2024-09-09 20:26:52 +0200

    ?

--- .github/scripts/coursier-snapshot.sh
+++ .github/scripts/coursier-snapshot.sh
@@ -6,10 +6,13 @@ if ! test -d coursier; then
   cd coursier
   git fetch --tags
   git submodule update --init --recursive
-  ./mill -i 'core.jvm[2.13.12].publishLocal' +\
-    'cache.jvm[2.13.12].publishLocal' +\
-    'proxy-setup.publishLocal' +\
-    'util.jvm[2.13.12].publishLocal' +\
-    'coursier.jvm[2.13.12].publishLocal'
   cd -
 fi
+
+cd coursier
+./mill -i 'core.jvm[2.13.12].publishLocal' +\
+  'cache.jvm[2.13.12].publishLocal' +\
+  'proxy-setup.publishLocal' +\
+  'util.jvm[2.13.12].publishLocal' +\
+  'coursier.jvm[2.13.12].publishLocal'
+cd -
