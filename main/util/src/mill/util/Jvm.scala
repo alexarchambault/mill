8eadbef7ab11a09c6640299eb3b10bb8c8420147
Alex Archambault <alexandre.archambault@gmail.com>
2025-04-11 01:15:22 +0200

    Make JvmCache cache stuff more reproducible
    More independent of the state of the archive cache in particular

--- main/util/src/mill/util/Jvm.scala
+++ main/util/src/mill/util/Jvm.scala
@@ -627,6 +627,9 @@ object Jvm {
       .withIndex(jvmIndex0(ctx, coursierCacheCustomizer, jvmIndexVersion))
     val javaHome = JavaHome()
       .withCache(jvmCache)
+      // when given a version like "17", always pick highest version in the index
+      // rather than the highest already on disk
+      .withUpdate(true)
     val file = javaHome.get(id).unsafeRun()(coursierCache0.ec)
     Result.Success(os.Path(file))
 
