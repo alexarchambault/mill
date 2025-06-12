774c3f3f9dab291e385bf8d9e6bf2bc56cd48110
Alex Archambault <alexandre.archambault@gmail.com>
2025-06-12 21:36:22 +0200

    Disable colors more forcefully in Scala 3

--- integration/ide/bsp-server/src/BspServerTests.scala
+++ integration/ide/bsp-server/src/BspServerTests.scala
@@ -470,16 +470,14 @@ object BspServerTests extends UtestIntegrationTestSuite {
               (85, 17),
               (85, 22),
               "WARNING",
-              // not sure why we still have colors here
-              "\u001b[33mmethod\u001b[0m \u001b[35mthing\u001b[0m in \u001b[33mobject\u001b[0m \u001b[35mdiag\u001b[0m is deprecated since 0.0.1: deprecated"
+              "method thing in object diag is deprecated since 0.0.1: deprecated"
             ))),
             Left(()),
             Right(Seq(SimpleDiagnostic(
               (85, 17),
               (85, 22),
               "WARNING",
-              // not sure why we still have colors here
-              "\u001b[33mmethod\u001b[0m \u001b[35mthing\u001b[0m in \u001b[33mobject\u001b[0m \u001b[35mdiag\u001b[0m is deprecated since 0.0.1: deprecated"
+              "method thing in object diag is deprecated since 0.0.1: deprecated"
             )))
           ),
         (os.sub / "diag", os.sub / "diag/src/DiagCheck.scala") ->
