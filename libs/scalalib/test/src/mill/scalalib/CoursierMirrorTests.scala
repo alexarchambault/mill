4da87e4ca74452a8d8fbda68426c462c418a0932
Alex Archambault <alexandre.archambault@gmail.com>
2025-08-19 16:47:31 +0200

    Re-enable mirror test

--- libs/scalalib/test/src/mill/scalalib/CoursierMirrorTests.scala
+++ libs/scalalib/test/src/mill/scalalib/CoursierMirrorTests.scala
@@ -1,6 +1,6 @@
 package mill.scalalib
 
-// import coursier.cache.FileCache
+import coursier.cache.FileCache
 import mill.api.Discover
 import mill.testkit.{TestRootModule, UnitTester}
 import mill.util.TokenReaders.*
@@ -20,20 +20,18 @@ object CoursierMirrorTests extends TestSuite {
 
   def tests: Tests = Tests {
     sys.props("coursier.mirrors") = (resourcePath / "mirror.properties").toString
-    test("readMirror") - UnitTester(CoursierTest, resourcePath).scoped { /*eval*/ _ =>
-      // Doesn't pass when other tests are running before/after in the same JVM
-      //      val Right(result) = eval.apply(CoursierTest.core.compileClasspath): @unchecked
-      //      val cacheRoot = os.Path(FileCache().location)
-      //      val cp = result.value
-      //        .map(_.path)
-      //        .filter(_.startsWith(cacheRoot))
-      //        .map(_.relativeTo(cacheRoot).asSubPath)
-      //      assert(cp.exists(f => f.last.startsWith("scala-library-") && f.last.endsWith(".jar")))
-      //      pprint.log(cp)
-      //      val centralReplaced = cp.forall { f =>
-      //        f.startsWith(os.sub / "https/repo.maven.apache.org/maven2")
-      //      }
-      //      assert(centralReplaced)
+    test("readMirror") - UnitTester(CoursierTest, resourcePath).scoped { eval =>
+      val Right(result) = eval.apply(CoursierTest.core.compileClasspath): @unchecked
+      val cacheRoot = os.Path(FileCache().location)
+      val cp = result.value
+        .map(_.path)
+        .filter(_.startsWith(cacheRoot))
+        .map(_.relativeTo(cacheRoot).asSubPath)
+      assert(cp.exists(f => f.last.startsWith("scala-library-") && f.last.endsWith(".jar")))
+      val centralReplaced = cp.forall { f =>
+        f.startsWith(os.sub / "https/repo.maven.apache.org/maven2")
+      }
+      assert(centralReplaced)
     }
   }
 }
