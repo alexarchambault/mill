e5774c3d0133836f7ab6e4b34fd2ce285cdce2bf
Alex Archambault <alexandre.archambault@gmail.com>
2025-09-05 17:38:35 +0200

    more

--- integration/feature/cs-proxy/src/CsProxyTests.scala
+++ integration/feature/cs-proxy/src/CsProxyTests.scala
@@ -1,91 +1,156 @@
 package mill.integration
 
-import mill.testkit.UtestIntegrationTestSuite
+import mill.testkit.{IntegrationTester, UtestIntegrationTestSuite}
 import utest._
 
 object CsProxyTests extends UtestIntegrationTestSuite {
 
   def isCI = System.getenv("CI") != null
 
-  val tests: Tests = Tests {
-    test("proxy") - integrationTest { tester =>
-      import tester._
+  def proxyTest(
+      wrongExtraEnv: (os.Path, Int) => Map[String, String],
+      rightExtraEnv: (os.Path, Int) => Map[String, String],
+      reset: IntegrationTester => Unit
+  ): Unit = integrationTest { tester =>
+    import tester._
 
-      val proxyPort = TestAuthProxy.randomPort()
+    val proxyPort = TestAuthProxy.randomPort()
 
-      val okM2Dir = workspacePath / "tmp/m2-ok"
-      os.write(okM2Dir / "settings.xml", TestAuthProxy.m2Settings(proxyPort), createFolders = true)
-      val nopeM2Dir = workspacePath / "tmp/m2-nope"
-      os.write(
-        nopeM2Dir / "settings.xml",
-        TestAuthProxy.m2Settings(proxyPort, "wrong", "nope"),
-        createFolders = true
+    val csCache = workspacePath / "tmp/cache"
+
+    TestAuthProxy.withAuthProxy(workspacePath, port = proxyPort) {
+
+      // We test the Mill launcher first
+
+      System.err.println("First wrong check")
+
+      // with the wrong proxy credentials, downloads must fail
+      val wrongCredentialsRes = eval(
+        "version",
+        env = Map("COURSIER_CACHE" -> csCache.toString) ++ wrongExtraEnv(workspacePath, proxyPort),
+        check = false
+      )
+      assert(wrongCredentialsRes.exitCode == 1)
+      // assert(wrongCredentialsRes.err.contains("Error downloading io.get-coursier.jvm.indices"))
+      assert(wrongCredentialsRes.err.contains("Unable to tunnel through proxy."))
+      assert(wrongCredentialsRes.err.contains("407 Proxy Authentication Required"))
+      pprint.err.log(wrongCredentialsRes)
+
+      System.err.println("Reset")
+      reset(tester)
+
+      System.err.println("First right check")
+
+      // but they should succeed with the right ones
+      eval(
+        "version",
+        env = Map("COURSIER_CACHE" -> csCache.toString) ++ rightExtraEnv(workspacePath, proxyPort),
+        check = true,
+        stderr = if (isCI) os.Pipe else os.Inherit
       )
 
-      val csCache = workspacePath / "tmp/cache"
+      System.err.println("Reset")
+      reset(tester)
 
-      TestAuthProxy.withAuthProxy(workspacePath, port = proxyPort) {
+      // Now we test javalib / scalalib
 
-        // We test the Mill launcher first
+      System.err.println("Second wrong check")
 
-        // with the wrong proxy credentials, downloads must fail
-        val wrongCredentialsRes = eval(
-          "version",
-          env = Map(
-            "COURSIER_CACHE" -> csCache.toString,
-            "CS_MAVEN_HOME" -> nopeM2Dir.toString
-          ),
-          check = false
+      // with the wrong proxy credentials, downloads must fail
+      val wrongCredentialsCompileRes = eval(
+        ("show", "compileClasspath"),
+        env = Map("COURSIER_CACHE" -> csCache.toString) ++ wrongExtraEnv(workspacePath, proxyPort),
+        check = false
+      )
+      assert(wrongCredentialsCompileRes.exitCode == 1)
+      assert(wrongCredentialsCompileRes.err.contains("Unable to tunnel through proxy."))
+      assert(wrongCredentialsCompileRes.err.contains("407 Proxy Authentication Required"))
+      pprint.err.log(wrongCredentialsCompileRes)
+
+      System.err.println("Reset")
+      reset(tester)
+
+      System.err.println("Second right check")
+
+      // but they should succeed with the right ones
+      eval(
+        ("show", "compileClasspath"),
+        env = Map("COURSIER_CACHE" -> csCache.toString) ++ rightExtraEnv(workspacePath, proxyPort),
+        check = true,
+        stderr = if (isCI) os.Pipe else os.Inherit
+      )
+
+      ()
+    }
+  }
+
+  val tests: Tests = Tests {
+    test("m2settings") {
+      proxyTest(
+        wrongExtraEnv = { (workspacePath, proxyPort) =>
+          val nopeM2Dir = workspacePath / "tmp/m2-nope"
+          os.write.over(
+            nopeM2Dir / "settings.xml",
+            TestAuthProxy.m2Settings(proxyPort, "wrong", "nope"),
+            createFolders = true
+          )
+          Map("CS_MAVEN_HOME" -> nopeM2Dir.toString)
+        },
+        rightExtraEnv = { (workspacePath, proxyPort) =>
+          val okM2Dir = workspacePath / "tmp/m2-ok"
+          os.write.over(
+            okM2Dir / "settings.xml",
+            TestAuthProxy.m2Settings(proxyPort),
+            createFolders = true
+          )
+          Map("CS_MAVEN_HOME" -> okM2Dir.toString)
+        },
+        reset = { tester =>
+          import tester._
+          // Need to shutdown the daemon to reset JDK internal HTTP stuff
+          eval("shutdown", check = true, stderr = os.Inherit)
+        }
+      )
+    }
+
+    test("java props") {
+      def jvmOpts(proxyPort: Int, user: String, password: String): Seq[String] =
+        Seq(
+          "-Dhttps.proxyProtocol=http",
+          "-Dhttps.proxyHost=localhost",
+          s"-Dhttps.proxyPort=$proxyPort",
+          s"-Dhttps.proxyUser=$user",
+          s"-Dhttps.proxyPassword=$password"
         )
-        assert(wrongCredentialsRes.exitCode == 1)
-        assert(wrongCredentialsRes.err.contains("Unable to tunnel through proxy."))
-        assert(wrongCredentialsRes.err.contains("407 Proxy Authentication Required"))
-
-        // but they should succeed with the right ones
-        eval(
-          "version",
-          env = Map(
-            "COURSIER_CACHE" -> csCache.toString,
-            "CS_MAVEN_HOME" -> okM2Dir.toString
-          ),
-          check = true,
-          stderr = if (isCI) os.Pipe else os.Inherit
-        )
-
-        // Need to shutdown the daemon to reset JDK internal HTTP stuff
-        eval("shutdown", check = true, stderr = os.Inherit)
-
-        // Now we test javalib / scalalib
-
-        // with the wrong proxy credentials, downloads must fail
-        val wrongCredentialsCompileRes = eval(
-          ("show", "compileClasspath"),
-          env = Map(
-            "COURSIER_CACHE" -> csCache.toString,
-            "CS_MAVEN_HOME" -> nopeM2Dir.toString
-          ),
-          check = false
-        )
-        assert(wrongCredentialsCompileRes.exitCode == 1)
-        assert(wrongCredentialsCompileRes.err.contains("Unable to tunnel through proxy."))
-        assert(wrongCredentialsCompileRes.err.contains("407 Proxy Authentication Required"))
-
-        // Need to shutdown the daemon to reset JDK internal HTTP stuff
-        eval("shutdown", check = true, stderr = os.Inherit)
-
-        // but they should succeed with the right ones
-        eval(
-          ("show", "compileClasspath"),
-          env = Map(
-            "COURSIER_CACHE" -> csCache.toString,
-            "CS_MAVEN_HOME" -> okM2Dir.toString
-          ),
-          check = true,
-          stderr = if (isCI) os.Pipe else os.Inherit
-        )
-
-        ()
-      }
+      proxyTest(
+        wrongExtraEnv = { (workspacePath, proxyPort) =>
+          if (!os.exists(workspacePath / "build.mill.orig"))
+            os.copy(workspacePath / "build.mill", workspacePath / "build.mill.orig")
+          os.write.over(
+            workspacePath / "build.mill",
+            s"//| mill-jvm-opts: [${jvmOpts(proxyPort, "wrong", "nope").map("\"" + _ + "\"").mkString(", ")}]" +
+              System.lineSeparator() +
+              os.read(workspacePath / "build.mill.orig")
+          )
+          Map.empty
+        },
+        rightExtraEnv = { (workspacePath, proxyPort) =>
+          if (!os.exists(workspacePath / "build.mill.orig"))
+            os.copy(workspacePath / "build.mill", workspacePath / "build.mill.orig")
+          os.write.over(
+            workspacePath / "build.mill",
+            s"//| mill-jvm-opts: [${jvmOpts(proxyPort, "jack", "insecure").map("\"" + _ + "\"").mkString(", ")}]" +
+              System.lineSeparator() +
+              os.read(workspacePath / "build.mill.orig")
+          )
+          Map.empty
+        },
+        reset = { tester =>
+          import tester._
+          // Need to shutdown the daemon to reset JDK internal HTTP stuff
+          eval("shutdown", check = true, stderr = os.Inherit)
+        }
+      )
     }
   }
 }
