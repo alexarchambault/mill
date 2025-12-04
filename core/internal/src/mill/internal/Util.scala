e76913e6a00a09809d0bb6a72e0e62eafd026339
Alex Archambault <alexandre.archambault@gmail.com>
2025-12-04 23:55:20 +0100

    fixup

--- core/internal/src/mill/internal/Util.scala
+++ core/internal/src/mill/internal/Util.scala
@@ -176,22 +176,18 @@ object Util {
 
     val headerDataResOpt0 = headerDataResOpt match {
       case Result.Success(dataOpt) => dataOpt.map(Result.Success(_))
-      case Result.Failure(error) => Some(Result.Failure(error))
+      case f: Result.Failure => Some(f)
     }
 
     def relativePath = scriptFile.relativeTo(mill.api.BuildCtx.workspaceRoot)
     given upickle.Reader[HeaderData] = HeaderData.headerDataReader(scriptFile)
 
     headerDataResOpt0.map { headerDataRes =>
-      headerDataRes.flatMap(parseYaml0(relativePath.toString, _, upickle.reader[HeaderData])).flatMap { parsed =>
-        try Result.Success(upickle.read[HeaderData](parsed))
-        catch {
-          case e: upickle.core.TraceVisitor.TraceException =>
-            Result.Failure(
-              s"Failed de-serializing config key ${e.jsonPath} in $relativePath: ${e.getCause.getMessage}"
-            )
-        }
-      }
+      headerDataRes.flatMap(parseYaml0(
+        relativePath.toString,
+        _,
+        upickle.reader[HeaderData]
+      ))
     }
   }
 
